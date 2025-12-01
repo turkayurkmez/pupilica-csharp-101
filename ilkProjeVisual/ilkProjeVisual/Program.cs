bbe4bb16439b87812258bf2eca70f5013008906a
türkay ürkmez <turkay.urkmez@outlook.com>
2025-12-01 19:15:53 +0300

    initialize repo

---
+++ ilkProjeVisual/ilkProjeVisual/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("İlk adımımız");
+
+     
+  
+
---
+++ ilkProjemiz/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace ilkProjemiz;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
