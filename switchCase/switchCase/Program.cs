c313714f15e31fe429b10e28a9f9183c1e159703
türkay ürkmez <turkay.urkmez@outlook.com>
2025-12-03 21:39:39 +0300

    Add traffic light logic and project setup
    Added a new solution file `switchCase.slnx` and a C# project file `switchCase.csproj` targeting `.NET 10.0` with `ImplicitUsings` and `Nullable` enabled.
    
    Implemented traffic light logic in `Program.cs` using both `if-else` and `switch` statements to handle user input for traffic light colors (`KIRMIZI`, `SARI`, `YESIL`). Added a placeholder `switch` statement for handling days of the week.

---
+++ switchCase/switchCase/Program.cs
@@ -0,0 +1,61 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Trafik ışığını giriniz:\nKIRMIZI\nSARI\nYESIL");
+string renk = Console.ReadLine();
+
+if (renk == "KIRMIZI")
+{
+    Console.WriteLine("DUR!");
+}
+else if (renk == "SARI")
+{
+    Console.WriteLine("DİKKAT!!!");
+}
+else if (renk == "YESIL")
+{
+    Console.WriteLine("GEÇ");
+}
+else
+{
+    Console.WriteLine("Bu ne biçim renk!?");
+}
+
+switch (renk)
+{
+    case "KIRMIZI":
+        Console.WriteLine("DUR!");
+        break;
+    case "SARI":
+        Console.WriteLine("DİKKAT!!!");
+        break;
+    case "YESIL":
+        Console.WriteLine("GEÇ");
+        break;
+
+
+    default:
+        Console.WriteLine("Bu ne biçim renk?");
+        break;
+}
+
+
+switch (DateTime.Now.DayOfWeek)
+{
+    case DayOfWeek.Sunday:
+        break;
+    case DayOfWeek.Monday:
+        break;
+    case DayOfWeek.Tuesday:
+        break;
+    case DayOfWeek.Wednesday:
+        break;
+    case DayOfWeek.Thursday:
+        break;
+    case DayOfWeek.Friday:
+        break;
+    case DayOfWeek.Saturday:
+        break;
+    default:
+        break;
+}
+
+
