000e0b0e276ce3af9ac535adb174a75a3f2ec567
türkay ürkmez <turkay.urkmez@outlook.com>
2025-12-10 20:05:17 +0300

    Add number guessing game console app (doWhileDongusu)
    Created new C# project targeting .NET 10.0. Implemented a console-based number guessing game where the computer selects a random 4-digit number and the user attempts to guess it, receiving feedback after each attempt. Added solution and project files.

---
+++ doWhileDongusu/doWhileDongusu/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+/*
+ * Amaç 1.  Bilgisayarın tuttuğu 4 haneli (1000,9999) sayıyı bulmak.
+ *         
+ *         1. Bilgisayar bir sayı tutar
+ *         2. Kullanıcı, sayıyı tahmin eder.
+ *         3. Tahmini sayı tutulandan büyükse "AŞAĞI" der ve 2. adıma geçer
+ *         4.                         küçükse "YUKARI" der ve 2. adıma geçer 
+ *         5.                         eşite   "BİLİNDİ" yazar ve biter.
+ *         
+ */
+
+Random random = new Random();
+int tutulanSayi = random.Next(1000, 10000);
+int denemeSayisi = 0;
+do
+{
+    Console.WriteLine("4 basamaklı Sayıyı tahmin edin:");
+    int tahmin = Convert.ToInt32(Console.ReadLine());
+    denemeSayisi++;
+    if (tahmin > tutulanSayi)
+    {
+        Console.WriteLine("AŞAĞI");
+
+    }
+    else if (tahmin < tutulanSayi)
+    {
+        Console.WriteLine("YUKARI");
+    }
+    else
+    {
+        Console.WriteLine($" {denemeSayisi} denemede BİLDİNİZ");
+        break;
+    }
+} while (true);
