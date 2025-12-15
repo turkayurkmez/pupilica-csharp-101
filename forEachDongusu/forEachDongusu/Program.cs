8572b10d6547a59f179ad0e8d05678377ac6931b
türkay ürkmez <turkay.urkmez@outlook.com>
2025-12-15 19:24:21 +0300

    Add foreach loop examples in new .NET 8 console app
    Created forEachDongusu solution and project. Added Program.cs with examples demonstrating foreach loops over a product list and an integer array, including calculation of the average. Included commented for loop alternative for comparison.

---
+++ forEachDongusu/forEachDongusu/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+List<string> urunler = new List<string>()
+{
+    "Kalem","Silgi","Kalemtraş","0.5 Uç"
+};
+
+//Amaç: her bir ürünü ekranda göstermek:
+
+//for (int i = 0; i < urunler.Count; i++)
+//{
+//    Console.WriteLine(urunler[i]);
+//}
+
+foreach (var urun in urunler)
+{
+    Console.WriteLine(urun);
+    //urunler.Add("test");
+}
+
+int[] numbers = { 78, 12, 9, 45 };
+
+int toplam = 0;
+foreach (var number in numbers)
+{
+    toplam += number;
+}
+
+double ortalama = toplam / numbers.Length;
+Console.WriteLine(ortalama);
