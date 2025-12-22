2f5f69f1c79e6b97c702d1ec7481c47d14aa77be
türkay ürkmez <turkay.urkmez@outlook.com>
2025-12-22 20:36:58 +0300

    Initial OOPIntro project with Urun class and demo usage
    Added new C# console app targeting .NET 10.0. Implemented the Urun class with properties for name, image, price (with validation), stock, and stock status. Program.cs demonstrates creating and using Urun objects, including property assignment and output. Added solution and project files.

---
+++ OOPIntro/OOPIntro/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+using OOPIntro;
+
+Console.WriteLine("Hello, World!");
+
+
+Urun urun2 = new Urun();
+
+urun2.Ad = "Yağmurluk";
+
+Console.WriteLine(urun1.Ad);
+
+//Console.WriteLine("Fiyat giriniz");
+//decimal fiyat = Convert.ToDecimal(Console.ReadLine());
+//if (fiyat<0)
+//{
+//    Console.Write("Fiyat negatif olamaz");
+//}
+//else
+//{
+//    urun1.Fiyat = fiyat;
+//}
+
+//urun1.SetFiyat(fiyat);
+//urun1.Fiyat = fiyat;
+Urun urun1 = new Urun();
+urun1.Ad = "Şemsiye";
+
+urun1.Fiyat = 15;
+Console.WriteLine(urun1.Fiyat.ToString())   ;
+urun1.Stok = 100;
+Console.WriteLine(urun1.Stok);
+
---
+++ OOPIntro/OOPIntro/Urun.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOPIntro
+{
+    class Urun
+    {
+
+
+        public string Ad { get; set; }
+
+        public string Resim { get; set; }
+
+        private decimal fiyat;
+
+        //public void SetFiyat(decimal fiyat)
+        //{
+        //    if (fiyat > 0)
+        //    {
+        //        this.fiyat = fiyat;
+        //    }
+        //    throw new Exception("fiyat negatif olamaz");
+        //}
+
+        //public decimal GetFiyat()
+        //{
+        //    return this.fiyat;
+        //}
+
+        public decimal Fiyat
+        {
+            get { return this.fiyat; }
+            set
+            {
+                if (value > 0)
+                {
+                    this.fiyat = value;
+                }
+                throw new ArgumentException("Fiyat negatif olamaz");
+            }
+        }
+
+        public int Stok { get; set; }
+
+        public bool StoktaVarmi
+        {
+            get
+            {
+                return Stok > 0;
+            }
+        }
+
+        public string Test { private get; set; }
+
+    }
+}
