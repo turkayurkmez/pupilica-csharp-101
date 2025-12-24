d7eb2764b6c71e5ae38118aaefec2098f9ad14db
türkay ürkmez <turkay.urkmez@outlook.com>
2025-12-24 21:32:29 +0300

    Add BuiltInInterfaces project with Student and ClassRoom
    Initialized C# console app targeting .NET 10.0. Added Student and ClassRoom classes implementing IComparable and IEnumerable for sorting and iteration. Demo usage in Program.cs.

---
+++ BuiltInInterfaces/BuiltInInterfaces/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BuiltInInterfaces;
+
+Console.WriteLine("Hello, World!");
+
+ClassRoom classRoom = new ClassRoom();
+classRoom.Add(new Student { Id = 1, Name = "Zeynep", Score = 75 });
+classRoom.Add(new Student { Id = 2, Name = "Elnur", Score = 80 });
+classRoom.Add(new Student { Id = 3, Name = "Elif", Score = 78 });
+
+classRoom.Sort();
+
+foreach (var item in classRoom)
+{
+    Console.WriteLine($"{item.Name} - {item.Score}");
+}
---
+++ BuiltInInterfaces/BuiltInInterfaces/Student.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuiltInInterfaces
+{
+    public class Student : IComparable<Student>
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Score { get; set; }
+
+        public int CompareTo(Student? other)
+        {
+            if (Score > other.Score)
+            {
+                return 1;
+            }
+            else if (Score < other.Score)
+            {
+                return -1;
+            }
+            return 0;
+        }
+    }
+
+    public class ClassRoom : IEnumerable<Student>
+    {
+        private List<Student> students = new List<Student>();
+
+        public void Add(Student student)
+        {
+            students.Add(student);
+        }
+
+        public void Sort()
+        {
+            students.Sort();
+        }
+
+        public List<Student> GetStudents() { 
+            return students;
+        }
+
+        public IEnumerator<Student> GetEnumerator()
+        {
+            foreach (var item in students)
+            {
+                yield return item;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
